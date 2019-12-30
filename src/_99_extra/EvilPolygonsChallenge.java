e0143c0ee4eb162826c34bad3fe8b21245b89e0e
League Student <league@iMac-16.attlocal.net>
2019-12-29 16:00:12 -0800

    new

--- src/_99_extra/EvilPolygonsChallenge.java
+++ src/_99_extra/EvilPolygonsChallenge.java
@@ -14,17 +14,39 @@ public class EvilPolygonsChallenge {
 
 		//2. Set the speed to 100
 		walle.setSpeed(100);
+		
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+		int colorChoice=JOptionPane.showOptionDialog(null, "Which color do you prefer?", "Polygons", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Blue","Red", "Green", "Orange"}, 0);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		if(colorChoice == 1) {
-			walle.setPenColor(Color BLUE);
+		if(colorChoice == 0) {
+			walle.setPenColor(Color.BLUE);
+		}
+		else if(colorChoice == 1) {
+			walle.setPenColor(Color.RED);
+		}
+		else if(colorChoice == 2) {
+			walle.setPenColor(Color.GREEN);
+		}
+		else if(colorChoice == 3) {
+			walle.setPenColor(Color.ORANGE);
 		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String x = JOptionPane.showInputDialog(null, "How many polygons do you want drawn?");
+		int y = Integer.parseInt(x);
 		//5. Use the robot to draw the number of polygons the user requested.
 		
+		for(int i = 0;i<y;i++) {
+			for(int j = 0;j<10;j++) {
+				walle.penDown();
+				walle.turn(36);
+				walle.move(20);
+			}
+			walle.penUp();
+			walle.turn(90);
+			walle.move(100);
+			walle.turn(-90);
+		}
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
